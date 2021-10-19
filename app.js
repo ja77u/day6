814373e2a2b2dbe4f5177c406bddbf5b1f95411a
niko <n.huttunen89@gmail.com>
2021-10-19 15:31:24 +0300

    book ja customer tehty

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bookRouter = require('./routes/book');
+var customerRouter = require('./routes/customer');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+const helmet = require('helmet');
+const cors = require('cors');
 
+app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/customer', customerRouter);
 
 module.exports = app;
+
+
