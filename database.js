9bdd1d1339e6d91d32b0b73f6dc3ac42b6e29339
niko <n.huttunen89@gmail.com>
2021-10-20 10:17:12 +0300

    valmis herokua varten

--- database.js
+++ database.js
@@ -1,8 +1,5 @@
 const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'library'
-});
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = 'mysql://netuser:netpass@localhost:3306/librarycreateConnection(process.env.SQL_SERVER)';
 module.exports = connection;
