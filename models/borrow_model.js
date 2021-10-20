b113c9af10e76b97c2bcec95259d5e40b4cd1b41
niko <n.huttunen89@gmail.com>
2021-10-20 13:17:23 +0300

    päivät formatoitu

--- models/borrow_model.js
+++ models/borrow_model.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 const borrow = {
   getById: function(id, callback) {
-    return db.query('select * from borrow where id_borrow=?', [id], callback);
+    return db.query('select DATE_FORMAT(borrow_date, "%d.%m.%Y"),DATE_FORMAT(return_date, "%d.%m.%Y")as palautus_pvm, id_book, id_customer from borrow where id_borrow=?', [id], callback);
   },
   getAll: function(callback) {
     return db.query('select * from borrow', callback);
